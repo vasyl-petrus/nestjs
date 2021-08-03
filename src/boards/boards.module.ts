23d81d330f77c66d0d509dcb5c8f94c48328a3fb
Vasyl Petrus <petrusvasyl.j@gmail.com>
2021-08-03 17:30:15 +0300

    board entitie

---
+++ src/boards/boards.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
+import { Board, BoardSchema } from './board.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Board.name, schema: BoardSchema }]),
+  ],
+  controllers: [BoardsController],
+  providers: [BoardsService],
+})
+export class BoardspModule {}
