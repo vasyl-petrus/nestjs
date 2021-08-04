7fb8511f1dfa0f0cd94000f50ac47855eac5291a
Vasyl Petrus <petrusvasyl.j@gmail.com>
2021-08-04 18:34:24 +0300

    fix token user type

--- src/token/token.dto.ts
+++ src/token/token.dto.ts
@@ -1,9 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { Schema as MongooseSchema } from 'mongoose';
 
 @InputType()
 export class UserTokenDto {
   @Field()
-  user_id: number;
+  user_id: MongooseSchema.Types.ObjectId;
 
   @Field()
   token: string;
